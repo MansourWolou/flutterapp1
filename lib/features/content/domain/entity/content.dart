e45dc9429c9e7bb4594f569c05d2e2a138bfcdbe
MansourWolou <woloumansour@gmail.com>
2023-09-25 13:19:01 +0200

    adding Equatable

--- lib/features/content/domain/entity/content.dart
+++ lib/features/content/domain/entity/content.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
 enum MediaType {
@@ -7,18 +8,21 @@ enum MediaType {
   sticker,
 }
 
-class Content {
+class Content extends Equatable{
   final Widget content; // img | circularIndicator
   final String description;
   final MediaType type;
   final int fileId;
   final List<String> tagList;
 
-  Content(
+  const Content(
     this.content,
     this.description,
     this.type,
     this.fileId,
     this.tagList
     );
+    
+      @override
+      List<Object?> get props => [content,tagList,fileId];
 }
